fba6d8c5b88c7714895acd53170923d58a85e7b9
oloza <olozra@gmail.com>
2025-06-28 12:47:27 -0400

    tu primer componente

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>reactJS</h1>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  return (
+    <div>
+      <h1>hola mark</h1>
+      <p>biltrumita</p>
+    </div>
+  );
+}
+root.render(
+  <div>
+    <Greeting />
+    <Greeting />
+  </div>
+);
