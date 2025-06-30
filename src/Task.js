fff4424b460b11426399a317e156fbad2c2ab9ff
oloza <olozra@gmail.com>
2025-06-30 15:23:42 -0400

    tipos de componentes

--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,10 @@
 import "./task.css";
+
 export function TaskCard(props) {
   console.log(props.ready);
   return (
     <div className="card">
-      <h1>myFirst task</h1>
+      <h1>Task</h1>
       {/* <span style={props.ready ? {backgroundColor:"green"} : {backgroundColor:"red"}}>{props.ready ? "Task completed" : "Task pending"}</span> */}
       <span className={props.ready?'bg-green':'bg-red'}>{props.ready ? "Task completed" : "Task pending"}</span>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import { UserCard } from "./Gretting";
 import { Button } from "./Button";
 import { TaskCard } from "./Task";
-
+import { Saludar } from "./Saludar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
    <TaskCard ready={false} />
+   <Saludar />
   </>
 );
